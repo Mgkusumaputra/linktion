b6fa3ea1c25208894594bbea78d93b8ef94dfd00
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2024-07-20 10:27:18 +0700

    feat: initialize project

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  semi: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['clsx'],
+  tailwindConfig: './tailwind.config.ts',
+};
