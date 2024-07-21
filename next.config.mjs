64f95723e83f5bc3169963e6e9ff925a5fef9ee5
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2024-07-21 16:05:17 +0700

    feat(notion): adding the rest notion integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      //   Available hostname for icon URL
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        pathname: '/attachments/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.discordapp.net',
+        pathname: '/attachments/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
