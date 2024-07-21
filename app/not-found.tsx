1cf91f66c03dd9bda61aeeb003375a02c330176c
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2024-07-21 11:42:46 +0700

    feat(notfoundpage): adding not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+export default function NotFound() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const [seconds, setSeconds] = useState(3);
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setSeconds((prevSeconds) => {
+        if (prevSeconds <= 1) {
+          clearInterval(countdown);
+          router.push('/');
+        }
+        return prevSeconds - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(countdown);
+  }, [router]);
+
+  return (
+    <div className='flex h-screen w-screen items-center justify-center gap-2 text-center'>
+      <div>
+        <h1 className='text-xl font-normal text-[#F2F3F4]'>
+          <span className='underline-offset-3 font-semibold underline'>
+            {pathname}
+          </span>{' '}
+          is not found
+        </h1>
+        <p className='text-xs font-normal text-[#E5E7E9]'>
+          Redirecting in {seconds} seconds...
+        </p>
+      </div>
+    </div>
+  );
+}
