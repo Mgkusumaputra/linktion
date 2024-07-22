3586f3b8b24af4909d7200037bf64dc7efbd8ddb
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2024-07-22 12:49:21 +0700

    chore(deps): migrate eslint 8 to 9

---
+++ eslint.config.js
@@ -0,0 +1,69 @@
+module.exports[
+  {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    plugins: ['@typescript-eslint', 'simple-import-sort', 'unused-imports'],
+    extends: [
+      'eslint:recommended',
+      'next',
+      'next/core-web-vitals',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+    rules: {
+      'no-unused-vars': 'off',
+      'no-console': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'react/no-unescaped-entities': 'off',
+
+      'react/display-name': 'off',
+      'react/jsx-curly-brace-presence': [
+        'warn',
+        { props: 'never', children: 'never' },
+      ],
+
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'warn',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+        },
+      ],
+      'simple-import-sort/exports': 'warn',
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            ['^@?\\w', '^\\u0000'],
+            ['^.+\\.s?css$'],
+            ['^@/lib', '^@/hooks'],
+            ['^@/data'],
+            ['^@/components'],
+            ['^@/'],
+            [
+              '^\\./?$',
+              '^\\.(?!/?$)',
+              '^\\.\\./?$',
+              '^\\.\\.(?!/?$)',
+              '^\\.\\./\\.\\./?$',
+              '^\\.\\./\\.\\.(?!/?$)',
+              '^\\.\\./\\.\\./\\.\\./?$',
+              '^\\.\\./\\.\\./\\.\\.(?!/?$)',
+            ],
+            ['^@/types'],
+            ['^'],
+          ],
+        },
+      ],
+    },
+    globals: {
+      React: true,
+      JSX: true,
+    },
+  }
+];
