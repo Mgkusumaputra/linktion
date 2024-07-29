da391e4dfdb5d9e9d44589674bcea2f4777c6eea
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2024-07-29 07:57:28 +0700

    chore: updating eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,69 +1,72 @@
-module.exports[
-  {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-    },
-    plugins: ['@typescript-eslint', 'simple-import-sort', 'unused-imports'],
-    extends: [
-      'eslint:recommended',
-      'next',
-      'next/core-web-vitals',
-      'plugin:@typescript-eslint/recommended',
-      'prettier',
-    ],
-    rules: {
-      'no-unused-vars': 'off',
-      'no-console': 'warn',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      'react/no-unescaped-entities': 'off',
+import { fixupPluginRules } from "@eslint/compat";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-      'react/display-name': 'off',
-      'react/jsx-curly-brace-presence': [
-        'warn',
-        { props: 'never', children: 'never' },
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      "typescript-eslint": tseslint,
+      "unused-imports": unusedImports,
+      react: reactPlugin,
+      "react-hooks": fixupPluginRules(reactHooks),
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "no-console": "warn",
+      "typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react/no-unescaped-entities": "off",
+
+      "react/display-name": "off",
+      "react/jsx-curly-brace-presence": [
+        "warn",
+        { props: "never", children: "never" },
       ],
 
-      '@typescript-eslint/no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'warn',
-      'unused-imports/no-unused-vars': [
-        'warn',
+      "typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "warn",
+      "unused-imports/no-unused-vars": [
+        "warn",
         {
-          vars: 'all',
-          args: 'after-used',
+          vars: "all",
+          args: "after-used",
         },
       ],
-      'simple-import-sort/exports': 'warn',
-      'simple-import-sort/imports': [
-        'warn',
+      "simple-import-sort/exports": "warn",
+      "simple-import-sort/imports": [
+        "warn",
         {
           groups: [
-            ['^@?\\w', '^\\u0000'],
-            ['^.+\\.s?css$'],
-            ['^@/lib', '^@/hooks'],
-            ['^@/data'],
-            ['^@/components'],
-            ['^@/'],
+            ["^@?\\w", "^\\u0000"],
+            ["^.+\\.s?css$"],
+            ["^@/lib", "^@/hooks"],
+            ["^@/data"],
+            ["^@/components"],
+            ["^@/"],
             [
-              '^\\./?$',
-              '^\\.(?!/?$)',
-              '^\\.\\./?$',
-              '^\\.\\.(?!/?$)',
-              '^\\.\\./\\.\\./?$',
-              '^\\.\\./\\.\\.(?!/?$)',
-              '^\\.\\./\\.\\./\\.\\./?$',
-              '^\\.\\./\\.\\./\\.\\.(?!/?$)',
+              "^\\./?$",
+              "^\\.(?!/?$)",
+              "^\\.\\./?$",
+              "^\\.\\.(?!/?$)",
+              "^\\.\\./\\.\\./?$",
+              "^\\.\\./\\.\\.(?!/?$)",
+              "^\\.\\./\\.\\./\\.\\./?$",
+              "^\\.\\./\\.\\./\\.\\.(?!/?$)",
             ],
-            ['^@/types'],
-            ['^'],
+            ["^@/types"],
+            ["^"],
           ],
         },
       ],
+      ...reactHooks.configs.recommended.rules,
     },
-    globals: {
-      React: true,
-      JSX: true,
-    },
-  }
+  },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  ...tseslint.configs.recommended,
 ];
